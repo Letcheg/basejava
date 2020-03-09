b3f152e61452e9ffd5ffac11f20160302c003051
Letcheg <apashuke@yandex.ru>
2020-03-09 09:59:16 +0300

    Lesson 4.1

--- test/com/basejava/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/basejava/webapp/storage/AbstractArrayStorageTest.java
@@ -1,7 +1,7 @@
 package com.basejava.webapp.storage;
 
-import com.basejava.webapp.exception.NotExistStorageException;
 import com.basejava.webapp.exception.ExistStorageException;
+import com.basejava.webapp.exception.NotExistStorageException;
 import com.basejava.webapp.exception.StorageException;
 import com.basejava.webapp.model.Resume;
 import org.junit.Assert;
@@ -19,7 +19,7 @@ public abstract class AbstractArrayStorageTest {
     public static final String UUID_1 = "uuid1";
     public static final String UUID_2 = "uuid3";
     public static final String UUID_3 = "uuid2";
-    public static final String UUID = "uu"; //for deleteNotExist and getNotExist
+    public static final String UUID_4 = "uuid4"; //for deleteNotExist and getNotExist
 
     @Before
     public void setUp() throws Exception {
@@ -53,60 +53,54 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     public void save() throws Exception {
-        for (int i = 4; i <= 10_000; i++) {
-            try {
-                storage.save(new Resume());
-            }
-            catch (StorageException e) {
-                Assert.fail("Array overflow error");
-            }
-        }
+        storage.save(new Resume());
+
     }
 
     @Test(expected = StorageException.class)
     public void saveOverflowError() throws Exception {
-        for (int i = 3; i <= 10_000; i++) {
-            storage.save(new Resume());
+        for (int i = storage.size(); i < AbstractArrayStorage.STORAGE_LIMIT; i++) {
+            try {
+                storage.save(new Resume());
+            } catch (StorageException e) {
+                Assert.fail("Array overflow error");
+            }
         }
+        storage.save(new Resume());
     }
 
     @Test(expected = ExistStorageException.class)
-    public void saveExistError() throws Exception {
+    public void saveExist() throws Exception {
         storage.save(new Resume(UUID_1));
     }
 
     @Test
     public void get() throws Exception {
-            storage.get(UUID_1);
+        storage.get(UUID_1);
     }
 
     @Test(expected = NotExistStorageException.class)
     public void getNotExist() throws Exception {
-        storage.get(UUID);
+        storage.get(UUID_4);
     }
 
     @Test(expected = NotExistStorageException.class)
     public void delete() throws Exception {
         storage.delete(UUID_1);
+        Assert.assertEquals(2, storage.size());
         storage.get(UUID_1);
     }
 
     @Test(expected = NotExistStorageException.class)
     public void deleteNotExist() throws Exception {
-        storage.delete(UUID);
+        storage.delete(UUID_4);
     }
 
     @Test
     public void getAll() throws Exception {
-        int checkSize = 0;
         Resume[] check = storage.getAll();
-        for (Resume r : storage.getAll()) {
-            checkSize++;
-        }
-        check[0].equals(storage.get(UUID_1));
-        check[1].equals(storage.get(UUID_2));
-        check[2].equals(storage.get(UUID_3));
-        Assert.assertEquals(storage.size(), checkSize);
+        Assert.assertArrayEquals(check, storage.getAll());
+        Assert.assertEquals(check.length, storage.getAll().length);
 
     }
 }
